65d05ef1e348c10dd55851faeeea9e04965ea58a
hakansaglam <hakan.saglam@qconnex.com>
2021-08-17 11:05:16 +0200

    eslint fix

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 
 @Module({
-  controllers: [TasksController]
+  controllers: [TasksController],
 })
 export class TasksModule {}

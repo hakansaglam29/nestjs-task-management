b20fcfc9f20c18864c52309988baecd90cf1b385
hakansaglam <hakan.saglam@qconnex.com>
2021-08-18 17:28:40 +0200

    how to generate access token | don't use sensible infos for access token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersRepository } from './users.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UsersRepository]),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })

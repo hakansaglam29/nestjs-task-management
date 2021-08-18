62a2908c1a695db1a412ae491e19260117227575
hakansaglam <hakan.saglam@qconnex.com>
2021-08-18 17:48:21 +0200

    json web token strategy added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { UsersRepository } from './users.repository';
 
 @Module({
@@ -17,7 +18,8 @@ import { UsersRepository } from './users.repository';
     }),
     TypeOrmModule.forFeature([UsersRepository]),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
